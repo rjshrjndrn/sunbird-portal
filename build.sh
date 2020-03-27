1c19d67b452ee292cc11177f6014d5a46311b041
Rajesh Rajendran <rjshrjndrn@gmail.com>
2020-03-27 09:30:54 +0530

    Enable docker cache and node installation in docker
    Signed-off-by: Rajesh Rajendran <rjshrjndrn@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 build_tag=$1
 name=player
 node=$2
@@ -16,22 +12,25 @@ commit_hash=$(git rev-parse --short HEAD)
 
 rm -rf src/app/app_dist/
 rm -rf src/app/player-dist.tar.gz
-nvm install 12.16.1
-nvm use 12.16.1
 cd src/app
-npm set progress=false
-npm install  --unsafe-perm
-npm run deploy
-cd app_dist
-npm i -g npm@6.13.4
-npm install --production  --unsafe-perm
-sed -i "/version/a\  \"buildHash\": \"${commit_hash}\"," package.json
-echo 'Compressing assets directory'
-cd ..
-tar -cvf player-dist.tar.gz app_dist
+# Running build inside docker
+echo "entering to node container"
+# Creating a docker volume for all the npm lib caching
+# if you want to clear the cache simply delete the volume
+# `docker volume rm node12`
+# Running the docker command with less priority as build takes time, don't want to choke other processes.
+docker run --rm -v node12_npm_cache:/var/lib/jenkins/.npm/ -v /etc/passwd:/etc/passwd -u `id -u`:`id -g` -v `pwd`:`pwd` circleci/node:12 echo "node version: "; \
+node -v ; \
+echo "npm version: "; \
+npm -v ; \
+npm set progress=false ; \
+nice npm install  --unsafe-perm ; \
+nice npm run deploy ; \
+cd app_dist ; \
+nice npm install --production  --unsafe-perm ; \
+sed -i "/version/a\  \"buildHash\": \"${commit_hash}\"," package.json ; \
 cd ../..
 
 docker build --no-cache --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\",\"commit_hash\" : \"${commit_hash}\", \"node_name\" : \"$node\"} > metadata.json
-
